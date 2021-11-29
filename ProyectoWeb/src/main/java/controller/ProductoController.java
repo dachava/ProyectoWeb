d76c5ff2c2360749fe0a68c1c42fb104c238a342
andresator <achavarria70005@ufide.ac.cr>
2021-11-28 19:12:28 -0600

    Modificacion de vista principal

--- ProyectoWeb/src/main/java/controller/ProductoController.java
+++ ProyectoWeb/src/main/java/controller/ProductoController.java
@@ -22,4 +22,56 @@ public class ProductoController extends Producto implements Serializable {
      public List<Producto> getProductos(){
         return ProductoGestion.getProductos();
     }
+    
+     
+     public String getCategoria(int cat){
+         
+         String categoria=new String();
+     
+         switch(cat) {
+            case 1:
+                categoria="Acetato";
+                break;
+            case 2:
+                categoria="CD";
+                 break;
+            case 3:
+                categoria="DVD";
+                 break;
+            case 4:
+                categoria="Casette";
+                 break;
+            case 5:
+                categoria="Digipak";
+                 break;
+            case 6:
+                categoria="T-Shirt";
+                break; 
+            case 7:
+                categoria="Accesorios";
+                break; 
+            default:
+                 categoria="Desconocido";
+                 break;
+        }
+    
+         return categoria;
+     }
+     
+     
+     public String getStock(int cantidad){
+         
+         String stockStatus = new String();
+         
+         if(cantidad <= 0){
+             stockStatus = "Agotado";
+         }
+         if(cantidad < 10){
+             stockStatus = "Pocas Unidades";
+         } else {
+             stockStatus = "Disponible";
+         }
+         return stockStatus;
+     }
+     
 }
